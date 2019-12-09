22434fbf39f6c7accdbb4278c7510f58301724ed
melhs99 <58580806+melhs99@users.noreply.github.com>
2019-12-09 15:03:12 +1100

    r update

--- code/make_xmls_GSS.R
+++ code/make_xmls_GSS.R
@@ -28,4 +28,14 @@ template_ucld_ultra <- readLines('GSS_template_ucld_ultra.xml')
 #Example to run this code
 
 #make_xml(seq_data, template_strict_dates, output)
-
+dir("../ebola_fasta_files/")
+input_files<-paste0("../ebola_fasta_files/", c("ebola_2018Public.fasta","ebola_early2014.fasta","ebola_liberia.fasta")) 
+input_data<-read.dna(input_files[1],format="fasta")
+source("make_xmls_GSS.R")
+input_files
+library(ape)
+input_data<-read.dna(input_files[1],format="fasta")
+make_xml(input_data,template_strict_dates,'ebola_2018Public_sc_het')
+make_xml(input_data,template_strict_ultra,'ebola_2018Public_sc_iso')
+make_xml(input_data,template_ucld_dates,'ebola_2018Public_ucld_het')
+make_xml(input_data,template_ucld_ultra,'ebola_2018Public_ucld_iso')
